17d8ab58cf58263c396a61af8ed1e43684598f5c
John Wiltberger <johwiltb@gmail.com>
2016-10-01 09:34:11 -0700

    Successfully  pulled data from herokuapp

--- module3/app.js
+++ module3/app.js
@@ -3,13 +3,38 @@
 
   angular.module('NarrowItDownApp', [])
   .controller("NarrowItDownController", NarrowItDownController)
-  .service("NarrowItDownService", NarrowItDownService);
+  .service("NarrowItDownService", NarrowItDownService)
+  .constant('JSONPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
-  function NarrowItDownService() {
+  NarrowItDownController.$inject = ['NarrowItDownService'];
+  function NarrowItDownController(NarrowItDownService) {
+     var menu = this;
+
+     var promise = NarrowItDownService.getMatchedMenuItems();
+
+     promise.then(function(response){
+        console.log("Win!");
+        console.log(response.data);
+     })
+     .catch(function(error){
+     	console.log("There was an error: " + error);
+   
+     });
+  
+  }
+
+
+  NarrowItDownService.$inject = ['$http', 'JSONPath'];
+  function NarrowItDownService($http, JSONPath) {
     var service = this;
 
-    service.getMatchedMenuItems(searchItems) {
+    service.getMatchedMenuItems = function() {
+      var response = $http({
+	 method: "GET",
+	 url: JSONPath
+      });
       
+      return response;
     }
   }
 })();
