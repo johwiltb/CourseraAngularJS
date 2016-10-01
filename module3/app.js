d8d0ddea27080a9a8bf784610b504c7ad3c926a0
John Wiltberger <johwiltb@gmail.com>
2016-10-01 16:10:19 -0700

    Completed service.  Working on custom directive

--- module3/app.js
+++ module3/app.js
@@ -4,37 +4,66 @@
   angular.module('NarrowItDownApp', [])
   .controller("NarrowItDownController", NarrowItDownController)
   .service("NarrowItDownService", NarrowItDownService)
-  .constant('JSONPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
+  .constant('JSONPath', "https://davids-restaurant.herokuapp.com/menu_items.json")
+  .directive('foundItems', FoundItems);
+
+  function FoundItems() {
+     var ddo = {
+     	template: '',
+     	scope: {
+     	   found_items: '<found-items',
+     	   onRemove: '&'
+	}
+     };
+  }
 
   NarrowItDownController.$inject = ['NarrowItDownService'];
   function NarrowItDownController(NarrowItDownService) {
      var menu = this;
+     
+     // FOR TESTING ONLY
+     var searchTerm = "chicken";
+     // END TESTING
+     menu.found = NarrowItDownService.getMatchedMenuItems(searchTerm);
 
-     var promise = NarrowItDownService.getMatchedMenuItems();
-
-     promise.then(function(response){
-        console.log("Win!");
-        console.log(response.data);
-     })
-     .catch(function(error){
-     	console.log("There was an error: " + error);
-   
-     });
-  
+     menu.removeItem = function(index) {
+     	menu.found.splice(index, 1);
+     };
   }
 
 
   NarrowItDownService.$inject = ['$http', 'JSONPath'];
   function NarrowItDownService($http, JSONPath) {
     var service = this;
+    var foundItems = [];
 
-    service.getMatchedMenuItems = function() {
-      var response = $http({
+
+    service.getMatchedMenuItems = function(searchTerm) {
+       var response = $http({
 	 method: "GET",
 	 url: JSONPath
       });
-      
-      return response;
+
+      response.then(function(data) {
+      	 console.log(data.data.menu_items.length);
+      	 var menuStuff = data.data.menu_items;
+      	 console.log(menuStuff.length);
+	
+	 for (var i = 0; i < menuStuff.length; i++) {
+	    if(menuStuff[i].description.indexOf(searchTerm) >= 0) {
+	       foundItems.push(menuStuff[i]);
+	    }
+	 }
+
+	 console.log(foundItems);
+	 console.log(foundItems.length);
+      	 // return found items
+      	 return foundItems;
+     	 
+      })
+      .catch(function(error) {
+      	 console.log("ERROR: " + error);
+      });
     }
   }
 })();
